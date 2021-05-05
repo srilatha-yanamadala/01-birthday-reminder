a9662ef162283e161bda15f97ae56e3c37b08563
Ravindranath Chowdary <55086334+srilatha-yanamadala@users.noreply.github.com>
2021-05-05 09:54:40 +0200

    first commit

---
+++ setup/src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Ravindranath',
+    age: 37,
+    image:
+      'https://scontent.fmmx3-1.fna.fbcdn.net/v/t1.18169-0/c13.0.511.511a/s552x414/29468686_1161875537288574_3536184646523347756_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=da31f3&_nc_ohc=dsdiCs7juZsAX_GGinj&_nc_ht=scontent.fmmx3-1.fna&tp=28&oh=73e399cfeb1c839a376df3053d69e51b&oe=60B76D3C',
+  },
+  {
+    id: 2,
+    name: 'Srilatha',
+    age: 30,
+    image:
+      'https://scontent.fmmx3-1.fna.fbcdn.net/v/t1.6435-0/c0.119.1152.1152a/s552x414/104264210_1875024895973631_5975238321400752251_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=da31f3&_nc_ohc=C9N4HALHjk4AX_N5V_9&_nc_ht=scontent.fmmx3-1.fna&tp=28&oh=8d789a4be1516859888ec45a60f41fae&oe=60B60C30',
+  },
+  {
+    id: 3,
+    name: 'Vikram Aditya',
+    age: 8,
+    image:
+      'https://scontent.fmmx3-1.fna.fbcdn.net/v/t1.18169-0/s552x414/19748649_1007596832716446_4920431691042163344_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=da31f3&_nc_ohc=Pw0Cq-OQEcQAX9BzH_j&_nc_ht=scontent.fmmx3-1.fna&tp=7&oh=5dce38afb9d7e4a8064d6a816e264914&oe=60B85EA4',
+  },
+  {
+    id: 4,
+    name: 'Mokshath Shasank',
+    age: 3,
+    image:
+      'https://scontent.fmmx3-1.fna.fbcdn.net/v/t1.6435-0/s552x414/39344141_1293726794103447_816561377043808256_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=da31f3&_nc_ohc=kDiN967KErcAX_ESytV&_nc_oc=AQlZlU6uPHbWgX1TF_NyKOA4JFcVtKtaHg7N5f_yoAtsdGtmZRTErTH2lFTyScADpYQ&_nc_ht=scontent.fmmx3-1.fna&tp=7&oh=7ce60cea1d2b73592f5b28f368b2971b&oe=60B994A6',
+  },
+  {
+    id: 5,
+    name: 'Shiva',
+    age: 29,
+    image:
+      'https://scontent.fmmx3-1.fna.fbcdn.net/v/t1.6435-0/s552x414/104226387_1875082812634506_9022655958983291432_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=da31f3&_nc_ohc=3QL1mZnrYusAX8jTUnw&_nc_ht=scontent.fmmx3-1.fna&tp=7&oh=2dbe81ca6cd6881adcbdfa3a6806d069&oe=60B5EFD0',
+  },
+];
